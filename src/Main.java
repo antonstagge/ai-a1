c8538bf3a03f8086dfcce7817b88328d092dd614
Anton Stagge <anton.stagge@winfallvr.com>
2018-09-05 12:44:12 +0200

    done with hmm0

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
-
-
 public class Main {
+
+    public static Matrix readMatix(Kattio io) {
+        int a_rows = io.getInt();
+        int a_cols = io.getInt();
+        double[] a_m = new double[a_rows*a_cols];
+        for (int i = 0; i< a_rows*a_cols; i++) {
+            double d = io.getDouble();
+            a_m[i] = d;
+        }
+        Matrix A = new Matrix(a_rows, a_cols, a_m);
+        return A;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hellow world");
-        Matrix m = new Matrix();
+        Kattio io = new Kattio(System.in, System.out);
+
+        Matrix A = readMatix(io);
+        Matrix B = readMatix(io);
+        Matrix pi = readMatix(io);
+
+        Matrix temp = pi.mult(A);
+        Matrix result = temp.mult(B);
+
+        StringBuilder ss = new StringBuilder();
+        ss.append(result.rows).append(" ").append(result.cols).append(" ");
+        for(int i = 0; i < result.cols*result.rows; i++){
+            ss.append(String.format("%.3f",result.mat[i])).append(" ");
+        }
+        System.out.println(ss.toString().trim());
+
+        io.close();
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -3,21 +3,64 @@ import java.util.ArrayList;
 public class Matrix {
     public int rows;
     public int cols;
-    public ArrayList<Double> mat;
+    public double[] mat;
 
-    public Matrix() {
-        rows = 0;
-        cols = 0;
-        mat = new ArrayList<Double>();
+    public Matrix(int r, int c) {
+        rows = r;
+        cols = c;
+        mat = new double[r*c];
     }
 
-    public Matrix(int r, int c, ArrayList<Double> m) {
+    public Matrix(int r, int c, double[] m) {
         rows = r;
         cols = c;
         mat = m;
     }
 
     public Double get(int x, int y) {
-        
+        if (x > rows-1 || x < 0 || y > cols-1 || y < 0) {
+            throw new IndexOutOfBoundsException("Not a valid index in matrix");
+        }
+        int index = x*cols + y;
+        return mat[index];
+    }
+
+    public void set(int x, int y, double val) {
+        if (x > rows-1 || x < 0 || y > cols-1 || y < 0) {
+            throw new IndexOutOfBoundsException("Not a valid index in matrix");
+        }
+        int index = x*cols + y;
+        mat[index]= val;
+    }
+
+    public Matrix mult(Matrix other) {
+        if (cols != other.rows) {
+            throw new IndexOutOfBoundsException("Matrices A amount of columns have to match matrix Bs amount of rows to be multiplied");
+        }
+        int new_cols = other.cols;
+        int new_rows = rows;
+        Matrix new_mat = new Matrix(new_rows, new_cols);
+
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < new_cols; y++) {
+                double value = 0.0;
+                for (int m = 0; m < cols; m++) {
+                    value += this.get(x, m)*other.get(m, y);
+                }
+                new_mat.set(x, y, value);
+            }
+        }
+        return new_mat;
+    }
+
+    public String toString() {
+        StringBuilder ss = new StringBuilder();
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < cols; y++) {
+                ss.append(this.get(x, y).toString()).append(", ");
+            }
+            ss.append("\n");
+        }
+        return  ss.toString();
     }
 }
